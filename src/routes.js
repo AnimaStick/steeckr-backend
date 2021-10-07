adf49baf4cf0cfa126b944e03738428c3c71ce1b
Gigademolidor <gabriel_braga_lagrotaria_de_oliveira@usp.br>
2021-10-07 17:15:20 -0300

    criacao de rota para exibir usuarios + configuracao banco

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
 const express = require('express');
 
+const connection = require('./config/database');
+
 const router = express.Router();
 
-router.get('/getUser', (request, response) => {
+router.get('/getUser', async (request, response) => {
+    try{
+        const res = await connection.query(`select * from "User" where name ~ 'inha'`);
+        response.status(200).json(res.rows);
+    }catch(e){
+        console.log(e);
+    }
     
-    response.status(200).json({message:"Não há usuário especificado"});
+
 })
 
 module.exports = router;
